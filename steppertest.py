671e11a9fb0c39ca3ca11d6383c2e2073ffb3fa2
Duncan <buzzbuzz.macdonald@gmail.com>
2019-06-06 12:46:05 -0700

    changes to steps

--- steppertest.py
+++ steppertest.py
@@ -30,24 +30,24 @@ stepper2.setSpeed(60)
 
 while True:
     print('Stepper 1 forward')
-    stepper1.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
+    stepper1.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # print('Stepper 1 backward')
     # stepper1.step(200, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.DOUBLE)
 
     # time.sleep(3)
 
     print('Stepper 2 forward')
-    stepper2.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
+    stepper2.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # print('Stepper 2 backward')
     # stepper2.step(200, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.DOUBLE)
 
-    # time.sleep(3)
+    time.sleep(3)
 
     print('Both steppers forward')
     # stepper1.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # stepper2.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
-    st1 = threading.Thread(target=stepperWorker, args=(stepper1, 200, Raspi_MotorHAT.FORWARD))
-    st2 = threading.Thread(target=stepperWorker, args=(stepper2, 200, Raspi_MotorHAT.FORWARD))
+    st1 = threading.Thread(target=stepperWorker, args=(stepper1, 100, Raspi_MotorHAT.FORWARD))
+    st2 = threading.Thread(target=stepperWorker, args=(stepper2, 100, Raspi_MotorHAT.FORWARD))
 
     st1.start()
     st2.start()
