b954208f9c64b82c93324d81d8efc0ae0ce5ba1b
Duncan <buzzbuzz.macdonald@gmail.com>
2019-06-24 14:24:16 -0700

    added async function and keepalive

--- steppertest.py
+++ steppertest.py
@@ -15,10 +15,18 @@ def turnOffMotors():
     hat.getMotor(3).run(Raspi_MotorHAT.RELEASE)
     hat.getMotor(4).run(Raspi_MotorHAT.RELEASE)
 
-def stepperWorker(stepper, steps, direction, style = Raspi_MotorHAT.DOUBLE):
+def stepperWorker(stepper, steps, direction = Raspi_MotorHAT.FORWARD, style = Raspi_MotorHAT.DOUBLE):
     print('stepping...')
     stepper.step(steps, direction, style)
     print('done stepping')
+
+    while stepper.isAlive():
+        pass
+
+def stepperWorkerAsync(stepper, steps, direction = Raspi_MotorHAT.FORWARD, style = Raspi_MotorHAT.DOUBLE):
+    thread = threading.Thread(target=stepper.step, args=(steps, direction, style))
+    thread.start()
+    return thread
 
 atexit.register(turnOffMotors)
 
@@ -30,27 +38,27 @@ stepper2.setSpeed(60)
 
 while True:
     print('Stepper 1 forward')
-    stepper1.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
+    # stepper1.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # print('Stepper 1 backward')
     # stepper1.step(200, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.DOUBLE)
+    stepperWorker(stepper1, 200)
 
     # time.sleep(3)
 
     print('Stepper 2 forward')
-    stepper2.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
+    # stepper2.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # print('Stepper 2 backward')
     # stepper2.step(200, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.DOUBLE)
-
+    stepperWorker(stepper2, 200)
     time.sleep(3)
 
     print('Both steppers forward')
     # stepper1.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # stepper2.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
-    st1 = threading.Thread(target=stepperWorker, args=(stepper1, 100, Raspi_MotorHAT.FORWARD))
-    st2 = threading.Thread(target=stepperWorker, args=(stepper2, 100, Raspi_MotorHAT.FORWARD))
+    # st1 = threading.Thread(target=stepperWorker, args=(stepper1, 100, Raspi_MotorHAT.FORWARD))
+    # st2 = threading.Thread(target=stepperWorker, args=(stepper2, 100, Raspi_MotorHAT.FORWARD))
+    stepperWorkerAsync(stepper1, 200)
+    stepperWorkerAsync(stepper2, 200)
 
-    st1.start()
-    st2.start()
-
+    print('restarting')
     time.sleep(3)
-    
