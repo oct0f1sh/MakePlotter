6a48d56e6493b3672ee3ca61bfc3a75f5823c98d
Duncan <buzzbuzz.macdonald@gmail.com>
2019-06-24 14:29:39 -0700

    changed structure

--- Raspi_MotorHAT/steppertest.py
+++ Raspi_MotorHAT/steppertest.py
@@ -15,43 +15,53 @@ def turnOffMotors():
     hat.getMotor(3).run(Raspi_MotorHAT.RELEASE)
     hat.getMotor(4).run(Raspi_MotorHAT.RELEASE)
 
-def stepperWorker(stepper, steps, direction, style = Raspi_MotorHAT.DOUBLE):
+def stepperWorker(stepper, steps, direction = Raspi_MotorHAT.FORWARD, style = Raspi_MotorHAT.DOUBLE):
     print('stepping...')
     stepper.step(steps, direction, style)
     print('done stepping')
 
+    # while stepper.isAlive():
+    #     pass
+
+def stepperWorkerAsync(stepper, steps, direction = Raspi_MotorHAT.FORWARD, style = Raspi_MotorHAT.DOUBLE):
+    thread = threading.Thread(target=stepper.step, args=(steps, direction, style))
+    thread.start()
+    return thread
+
 atexit.register(turnOffMotors)
 
-stepper1 = hat.getStepper(100, 1)
+stepper1 = hat.getStepper(200, 1)
 stepper1.setSpeed(60) # RPM
 
-stepper2 = hat.getStepper(100, 2)
+stepper2 = hat.getStepper(200, 2)
 stepper2.setSpeed(60)
 
 while True:
     print('Stepper 1 forward')
-    stepper1.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
+    # stepper1.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # print('Stepper 1 backward')
     # stepper1.step(200, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.DOUBLE)
+    stepperWorker(stepper1, 200)
 
     # time.sleep(3)
 
     print('Stepper 2 forward')
-    stepper2.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
+    # stepper2.step(100, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # print('Stepper 2 backward')
     # stepper2.step(200, Raspi_MotorHAT.BACKWARD, Raspi_MotorHAT.DOUBLE)
-
+    stepperWorker(stepper2, 200)
     time.sleep(3)
 
     print('Both steppers forward')
     # stepper1.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
     # stepper2.step(200, Raspi_MotorHAT.FORWARD, Raspi_MotorHAT.DOUBLE)
-    st1 = threading.Thread(target=stepperWorker, args=(stepper1, 100, Raspi_MotorHAT.FORWARD))
-    st2 = threading.Thread(target=stepperWorker, args=(stepper2, 100, Raspi_MotorHAT.FORWARD))
+    # st1 = threading.Thread(target=stepperWorker, args=(stepper1, 100, Raspi_MotorHAT.FORWARD))
+    # st2 = threading.Thread(target=stepperWorker, args=(stepper2, 100, Raspi_MotorHAT.FORWARD))
+    st1 = stepperWorkerAsync(stepper1, 200)
+    st2 = stepperWorkerAsync(stepper2, 200)
 
-    st1.start()
-    st2.start()
+    while st1.isAlive() or st2.isAlive():
+        pass
 
+    print('restarting')
     time.sleep(3)
-
-    // I'm an iOS Engineer with a passion for Python and hardware. Published two Swift CocoaPods Libraries for iOS Development. I built Desktop Buddy, a Raspberry Pi powered 32x32 LED matrix display platform that is integrated with Amazon Web Service's Internet of Things platform, along with an iPad companion app.
